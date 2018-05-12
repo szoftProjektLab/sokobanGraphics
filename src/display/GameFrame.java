1471593258535c63a2cd07416d82b60c60c204ef
IncredibleK <aron11111@gmail.com>
2018-05-12 14:59:20 +0200

    GameFrame SetTexture, mások javítása

--- src/display/GameFrame.java
+++ src/display/GameFrame.java
@@ -1,16 +1,37 @@
 package display;
-import java.awt.BorderLayout;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JSplitPane;
 import javax.swing.JLabel;
-import java.awt.GridLayout;
 
 public class GameFrame extends JFrame {
 
     private JPanel contentPane;
+    private String globalVariable = null;
+    private BufferedImage texture = null;
+    private JPanel mainPanel;
+    private JPanel labelGame;
+    int x, y;
 
+
+    private BufferedImage loadImage(){
+        if(globalVariable == null)
+            return null;
+        BufferedImage result = null;
+
+        try {
+            result = ImageIO.read(new File(globalVariable));
+        } catch (IOException e) {
+            System.out.println("Rossz volt!");
+        }
+        return result;
+    }
     /**
      * Create the frame.
      */
@@ -22,7 +43,7 @@ public class GameFrame extends JFrame {
         contentPane.setLayout(new BorderLayout(0, 0));
         setContentPane(contentPane);
 
-        JPanel mainPanel = new JPanel();
+        mainPanel = new JPanel();
         contentPane.add(mainPanel, BorderLayout.NORTH);
         mainPanel.setLayout(new BorderLayout(0, 0));
 
@@ -33,9 +54,28 @@ public class GameFrame extends JFrame {
         JLabel labelPlayer2 = new JLabel("<html>Player 2<br/>Score</html>");
         mainPanel.add(labelPlayer2,BorderLayout.EAST);
 
-        JPanel labelGame = new JPanel();
+        labelGame = new JPanel(){
+            @Override
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Dimension size = getSize();
+                if(texture == null)
+                    return;
+
+                g.drawImage(texture, x, y,size.width, size.height,x, y, texture.getWidth(), texture.getHeight(), null);
+            }
+        };
         contentPane.add(labelGame, BorderLayout.CENTER);
         labelGame.setLayout(new GridLayout(1, 0, 0, 0));
+    }
+    public void SetTexture(int x, int y, String path)
+    {
+        this.x = x;
+        this.y = y;
+        this.globalVariable = path;
+        labelGame.revalidate();
+        labelGame.repaint();
+
     }
 
 }
