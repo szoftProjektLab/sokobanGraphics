de4809f4076a71e55b461e1727edf6a8e0dc23ba
Zsocz <nagyzsolt@users.noreply.github.com>
2018-05-12 14:42:18 +0200

    main régi kód törlése

--- src/Main.java
+++ src/Main.java
@@ -18,48 +18,6 @@ public class Main {
                 }
             }
         });
-        /*Warehouse w = new Warehouse();
-
-        Field f1 = new Field();
-        Field f2 = new Wall();
-        f1.SetNeighbour(Direction.Right, f2);
-        Player p = new Player(3);
-        f1.Add(p);
-        p.StartMove(Direction.Right);
-
-        System.out.println("\n");
-
-        Field f3 = new Field();
-        Hole h1= new Hole();
-        f3.SetNeighbour(Direction.Right, h1);
-        Player p2 = new Player(3);
-        f3.Add(p2);
-        p2.StartMove(Direction.Right);
-
-        System.out.println("\n");
-
-        Field f11 = new Field();
-        Field f12 = new Wall();
-        f1.SetNeighbour(Direction.Right, f2);
-        Player p11 = new Player(3);
-        Player p12 = new Player(3);
-        f11.Add(p11); //Interactot hív
-        f12.Add(p12);
-        p11.StartMove(Direction.Right);
-
-        System.out.println("\n");*/
-
-        Box b= new Box();
-        ColouredBox cb1=new ColouredBox();
-        ColouredBox cb2=new ColouredBox();
-        Colours color1=new Colours();
-        Colours color2=new Colours();
-        cb1.SetColour(color1);
-        cb2.SetColour(color2);
-        if(cb1.GetColour()==cb2.GetColour()) System.out.println("True");
-        else System.out.println("False");
-
-        //Falra playert tol???
 
     }
 }
